70303aa85f00131cd37621d2e6a95798169b90fb
Jesse Clark <hello@developerjesse.com>
2021-11-18 14:45:26 +0200

    Format relayhub json.

--- src/components/RelayHub.tsx
+++ src/components/RelayHub.tsx
@@ -21,11 +21,19 @@ const RelayHub: React.FC<Interface> = () => {
 
   return (
     <div>
-      <h2>Relay Hub</h2>
+      <h2>Relay Hub Info</h2>
       <p><strong>URL</strong> {relayHubAddress}</p>
       <button onClick={getHubInfo} disabled={!!details}>get details from hub</button>
-      {details && (JSON.stringify(details))}
+      {details && (
+        <>
+          <h3>details</h3>
+          <pre style={{ width: '90%', textAlign: 'left' }}>
+            {JSON.stringify(details, null, 2)}
+          </pre>
+        </>
+      )}
       {error && <p><strong>{error}</strong></p>}
+      <hr />
     </div>
   )
 }
